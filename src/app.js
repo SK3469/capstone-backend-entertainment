0b8f9d508ba248b3163b374a7473103a49ec0d3c
sunil kumar <sunilk3469@gmail.com>
2025-02-24 18:13:47 +0530

    updaed

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin:'*',
+    origin:'https://capstone-backend-entertainment.onrender.com',
     methods: ["GET", "POST", "PUT", "DELETE"], //CRUD Oprations
     credentials: true
 }))
